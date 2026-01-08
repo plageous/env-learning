9ff74847883b7bcb6d0099eece63c6dff5c4b755
KJ <122484032+plageous@users.noreply.github.com>
2026-01-08 19:50:37 +0000

    increased gravity, modified colors

--- src/main.cpp
+++ src/main.cpp
@@ -7,28 +7,33 @@
 
 #define FLOOR (80 - 4)
 
-int main() {
+int main()
+{
     bn::core::init();
 
-    bn::backdrop::set_color(bn::color(15, 0, 0));
+    bn::backdrop::set_color(bn::color(0, 15, 0));
 
     auto dot = bn::sprite_items::dot.create_sprite(0, 0);
 
     bn::fixed speed = 1.5;
 
     bn::fixed dy = 0;
-    bn::fixed gravity = .03;
+    bn::fixed gravity = .10;
 
     bn::fixed jump_strength = 1;
 
-    while(true) {
-        if(bn::keypad::left_held()) {
+    while (true)
+    {
+        if (bn::keypad::left_held())
+        {
             dot.set_x(dot.x() - speed);
         }
-        if(bn::keypad::right_held()) {
+        if (bn::keypad::right_held())
+        {
             dot.set_x(dot.x() + speed);
         }
-        if(bn::keypad::a_pressed()) {
+        if (bn::keypad::a_pressed())
+        {
             dy -= jump_strength;
         }
 
@@ -36,7 +41,8 @@ int main() {
 
         dot.set_y(dot.y() + dy);
 
-        if(dot.y() > FLOOR) {
+        if (dot.y() > FLOOR)
+        {
             dot.set_y(FLOOR);
             dy = 0;
         }
